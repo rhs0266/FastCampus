5ac3690b9799b0b9d2a9443d41a63f583398faf0
Hoseok Ryu <dkflfkdeh@naver.com>
2021-07-08 16:16:06 +0900

    Update solution.java

--- 강의 자료/02-알고리즘/07~08-두 포인터/문제별 코드/2473-세 용액/solution.java
+++ 강의 자료/02-알고리즘/07~08-두 포인터/문제별 코드/2473-세 용액/solution.java
@@ -26,9 +26,9 @@ public class Main {
             int target = -A[i];
             int L = i + 1, R = N;
             while (L < R){  // L == R 인 상황이면 용액이 한 개 뿐인 것이므로, L < R 일 때까지만 반복한다.
-                if (best_sum > Math.abs((long)target + A[L] + A[R])) {
-                    best_sum = Math.abs((long)target + A[L] + A[R]);
-                    v1 = target;
+                if (best_sum > Math.abs(-(long)target + A[L] + A[R])) {
+                    best_sum = Math.abs(-(long)target + A[L] + A[R]);
+                    v1 = -target;
                     v2 = A[L];
                     v3 = A[R];
                 }
