1c155b499d33352325dc9708661f63a013eb7b57
Hoseok Ryu <dkflfkdeh@naver.com>
2022-10-13 14:23:19 +0900

    Update solution.py

--- 강의 자료/02-알고리즘/09~11-그래프 탐색/문제별 코드/7569-토마토/solution.py
+++ 강의 자료/02-알고리즘/09~11-그래프 탐색/문제별 코드/7569-토마토/solution.py
@@ -1,42 +1,46 @@
 import sys
 from collections import deque
 si = sys.stdin.readline
-M, N = list(map(int, si().split()))
+M, N, H = list(map(int, si().split()))
 a = []
-for n in range(N):
-    a.append(list(map(int, si().split())))
+for h in range(H):
+    a.append([])
+    for n in range(N):
+        a[h].append(list(map(int, si().split())))
             
-dist = [[-1 for m in range(M)] for n in range(N)]
+dist = [[[-1 for m in range(M)] for n in range(N)] for h in range(H)]
 
-dir = [(1, 0), (0, 1), (-1, 0), (0, -1)]
+dir = [(1, 0, 0), (-1, 0, 0), (0, 1, 0), (0, -1, 0), (0, 0, 1), (0, 0, -1)]
 
 q = deque()
-for n in range(N):
-    for m in range(M):
-        if a[n][m] == 1:
-            dist[n][m] = 0
-            q.append((n, m))
+for h in range(H):
+    for n in range(N):
+        for m in range(M):
+            if a[h][n][m] == 1:
+                dist[h][n][m] = 0
+                q.append((h, n, m))
 
 while q:
-    n, m = q.popleft()
-    for dn, dm in dir:
-        nn, nm = n + dn, m + dm
-        if nn < 0 or nm < 0 or nn >= N or nm >= M:
+    h, n, m = q.popleft()
+    for dh, dn, dm in dir:
+        nh, nn, nm = h + dh, n + dn, m + dm
+        if nh < 0 or nn < 0 or nm < 0 or nh >= H or nn >= N or nm >= M:
             continue
-        if dist[nn][nm] != -1:
+        if dist[nh][nn][nm] != -1:
             continue
-        if a[nn][nm] == -1:
+        if a[nh][nn][nm] == -1:
             continue
-        q.append((nn, nm))
-        dist[nn][nm] = dist[n][m] + 1
+        q.append((nh, nn, nm))
+        dist[nh][nn][nm] = dist[h][n][m] + 1
 
 ans = 0
 
-for n in range(N):
-    for m in range(M):
-        if a[n][m] == -1: continue
-        if dist[n][m] == -1: ans = -1
-        if ans == -1: continue
-        ans = max(ans, dist[n][m])
+for h in range(H):
+    for n in range(N):
+        for m in range(M):
+            if a[h][n][m] == -1: continue
+            if dist[h][n][m] == -1: ans = -1
+            if ans == -1: continue
+            ans = max(ans, dist[h][n][m])
 
 print(ans)
