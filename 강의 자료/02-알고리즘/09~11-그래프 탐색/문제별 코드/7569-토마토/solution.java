243ea712e419b969aa210dded5ec796cdf2e93b3
Hoseok Ryu <dkflfkdeh@naver.com>
2022-10-13 14:24:30 +0900

    Update solution.java

--- 강의 자료/02-알고리즘/09~11-그래프 탐색/문제별 코드/7569-토마토/solution.java
+++ 강의 자료/02-알고리즘/09~11-그래프 탐색/문제별 코드/7569-토마토/solution.java
@@ -6,63 +6,73 @@ public class Main {
     static FastReader scan = new FastReader();
     static StringBuilder sb = new StringBuilder();
 
-    static int N, M;
-    static int[][] a;
-    static int[][] dist;
-    static int[][] dir = {{1, 0}, {0, 1}, {-1, 0}, {0, -1}};
+    static int N, M, H;
+    static int[][][] dist, a;
+    static int[][] dir = { {1,0,0,},{-1,0,0},{0,1,0},{0,-1,0},{0,0,1}, {0,0,-1} };
     
     static void input() {
         M = scan.nextInt();
         N = scan.nextInt();
-        dist = new int[N][M];
-        a = new int[N][M];
-        for (int i = 0; i < N; i++) {
-            for (int j = 0; j < M; j++) {
-                a[i][j] = scan.nextInt();
-            }
-        }
-    }
-
-    static int bfs() {
-        Queue<Integer> Q = new LinkedList<>();
-        for (int i = 0; i < N; i++) {
-            for (int j = 0; j < M; j++) {
-                dist[i][j] = -1;
-                if (a[i][j] == 1) {  // 익은 토마토의 위치가 곧 시작점
-                    dist[i][j] = 0;
-                    Q.add(i);
-                    Q.add(j);
+        H = scan.nextInt();
+        a = new int[H + 1][N + 1][M + 1];
+        dist = new int[H + 1][N + 1][M + 1];
+        for (int h = 1; h <= H; h++) {
+            for (int i = 1; i <= N; i++) {
+                for (int j = 1; j <= M; j++) {
+                    a[h][i][j] = scan.nextInt();
                 }
             }
         }
-        
+    }
+
+    static void bfs() {
+        Queue<Integer> Q = new LinkedList<>();
+        for (int h = 1; h <= H; h++) {
+            for (int i = 1; i <= N; i++) {
+                for (int j = 1; j <= M; j++) {
+                    dist[h][i][j] = -1;
+                    if (a[h][i][j] == 1) {
+                        dist[h][i][j] = 0;
+                        Q.add(h); Q.add(i); Q.add(j);
+                    }
+                }
+            }
+        }
+
         // BFS 과정 시작
         while (!Q.isEmpty()) {
-            int x = Q.poll();
-            int y = Q.poll();
-            for (int k = 0; k < 4; k++) {
-                int nx = x + dir[k][0], ny = y + dir[k][1];
-                if (nx < 0 || ny < 0 || nx >= N || ny >= M) continue;  // 지도를 벗어나는 곳으로 가는가?
-                if (dist[nx][ny] != -1) continue;  // 이미 방문한 적이 있는 곳인가?
-                if (a[nx][ny] == -1) continue;  // 토마토가 들어있지 않는 칸
-                Q.add(nx);
-                Q.add(ny);
-                dist[nx][ny] = dist[x][y] + 1;
+            int h = Q.poll();
+            int i = Q.poll();
+            int j = Q.poll();
+            for (int k = 0; k < 6; k++) {
+                int nh = h + dir[k][0];
+                int ni = i + dir[k][1];
+                int nj = j + dir[k][2];
+                if (nh < 1 || ni < 1 || nj < 1 || nh > H || ni > N || nj > M) continue;
+                if (dist[nh][ni][nj] != -1) continue;
+                if (a[nh][ni][nj] == -1) continue;
+                dist[nh][ni][nj] = dist[h][i][j] + 1;
+                Q.add(nh); Q.add(ni); Q.add(nj);
             }
         }
-        int ans = 0;
-        for (int i = 0; i < N; i++) {
-            for (int j = 0; j < M; j++) {
-                if (a[i][j] == -1) continue;
-                if (dist[i][j] == -1) return -1;
-                ans = Math.max(ans, dist[i][j]);
-            }
-        }
-        return ans;
     }
 
     static void pro() {
-        System.out.println(bfs());
+        bfs();
+        int ans = 0;
+        for (int h = 1; h <= H; h++) {
+            for (int i = 1; i <= N; i++) {
+                for (int j = 1; j <= M; j++) {
+                    if (a[h][i][j] == -1) continue;
+                    if (dist[h][i][j] == -1) {
+                        System.out.println(-1);
+                        return;
+                    }
+                    ans = Math.max(ans, dist[h][i][j]);
+                }
+            }
+        }
+        System.out.println(ans);
     }
 
     public static void main(String[] args) {
